43e6451ef9731e1851de8f3794b3919275923a90
Vamsi Krishna Badri <badrivamsi@gmail.com>
2021-12-22 15:36:50 +0530

    Understood concepts of urls, views, and templates

---
+++ base/urls.py
@@ -0,0 +1,8 @@
+from typing import ValuesView
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('room/<str:pk>', views.room, name='room'),
+]
--- base/views.py
+++ base/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+rooms = [
+    {'id': 1, 'name': 'Lets learn python'},
+    {'id': 2, 'name': 'Lets learn django'},
+    {'id': 3, 'name': 'Lets learn Backend development'},
+]
+
+def home(request):
+    context = {'rooms': rooms}
+    return render(request, 'home.html', context)
+
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id']==int(pk):
+            room = i
+            break
+    context = {'room': room}
+    return render(request, 'room.html', context)
